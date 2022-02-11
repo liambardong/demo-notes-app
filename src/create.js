d0eae487dad3b2b22e9ff215ef40d2646ca9925a
Liam Bardong <ltb2@williams.edu>
2022-02-11 16:28:43 -0500

    Securing the API

--- src/create.js
+++ src/create.js
@@ -1,15 +1,18 @@
 import * as uuid from "uuid";
 
 import handler from "./util/handler";
-import dynamoDb from "./util/dynamoDb";
+import dynamoDb from "./util/dynamodb";
 
 export const main = handler(async (event) => {
+	console.log(event);
+	console.log(event.requestContext.authorizer.iam.cognitoIdentity.identityId);
+	console.log("after");
 	const data = JSON.parse(event.body);
 	const params = {
 		TableName: process.env.TABLE_NAME,
 		Item: {
 			// The attributes of the item to be created
-			userId: "123",
+			userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
 			noteId: uuid.v1(),
 			content: data.content,
 			attachment: data.attachment,
