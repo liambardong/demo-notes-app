2337b6287463aa50dbbe8e0fcd18f93a544ef676
Liam Bardong <ltb2@williams.edu>
2022-04-25 18:41:09 -0400

    Building our React app

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,6 @@
 const config = {
+	MAX_ATTACHMENT_SIZE: 5000000,
+	STRIPE_KEY:"pk_test_51KTVq5CY99melWT2aMzNrJvgW0fVyYNpV5oJ3XcfoLWrnI5BrPDeChX8MCtz4iVfbwJ8cY67xA2jnDreHMdQ5aSN00Fi9ne5Cq",
 	// Backend config
 	s3: {
 		REGION: process.env.REACT_APP_REGION,
@@ -6,11 +8,11 @@ const config = {
 	},
 	apiGateway: {
 		REGION: process.env.REACT_APP_REGION,
-		URL: process.env.REACT_APP_BUCKET,
+		URL: process.env.REACT_APP_API_URL,
 	},
 	cognito: {
 		REGION: process.env.REACT_APP_REGION,
-		USER_POOL_ID: process.env.REACT_APP_API_URL,
+		USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
 		APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
 		IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
 	},
